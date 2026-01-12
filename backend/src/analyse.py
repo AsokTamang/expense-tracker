2c97ed5ec2b153cd719a2027f29a873215ba42cf
AsokTamang <ashoktmg2058@gmail.com>
2026-01-12 14:58:19 -0500

    showing the data of expenses based on category in bar format

--- backend/src/analyse.py
+++ backend/src/analyse.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import sys
+import matplotlib.pyplot as plt
 
 projectroot = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 print(projectroot)
@@ -13,7 +14,8 @@ def get_datas(start_date, end_date):
         data = pd.read_sql('''SELECT category,SUM(amount) AS total FROM expenses
                            WHERE expense_date BETWEEN %s AND %s
                            GROUP BY category;''',  db,params=[start_date,end_date])  # here db is our sql connecting instance\
-        df = pd.DataFrame(data).plot(kind="bar")  # converting the data into dataframe using pd
+        df = pd.DataFrame(data).plot(kind="bar",x='category')  # converting the data into dataframe using pd
         return df
 print(get_datas('2024-08-01', '2024-09-02'))
+plt.show()
 
