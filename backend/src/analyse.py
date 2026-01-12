8336cf442ad78a7891570e80c81c20087fcdfa70
AsokTamang <ashoktmg2058@gmail.com>
2026-01-12 14:54:45 -0500

    analysing the expense based on category

--- backend/src/analyse.py
+++ backend/src/analyse.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import os
 import sys
-projectroot=os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+projectroot = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 print(projectroot)
-sys.path.insert(0,projectroot)   #here we are appending the current file dir into a PYTHONPATH
+sys.path.insert(0, projectroot)  # here we are appending the current file dir into a PYTHONPATH
 from db.database import get_connection
 
-with get_connection() as (cursor,db):
-    data= pd.read_sql('SELECT * FROM expenses',db)  #here db is our sql connecting instance\
-    df = pd.DataFrame(data) #converting the data into dataframe using pd
-    print(df)
 
+def get_datas(start_date, end_date):
+    with get_connection() as (cursor, db):
+        data = pd.read_sql('''SELECT category,SUM(amount) AS total FROM expenses
+                           WHERE expense_date BETWEEN %s AND %s
+                           GROUP BY category;''',  db,params=[start_date,end_date])  # here db is our sql connecting instance\
+        df = pd.DataFrame(data).plot(kind="bar")  # converting the data into dataframe using pd
+        return df
+print(get_datas('2024-08-01', '2024-09-02'))
 
