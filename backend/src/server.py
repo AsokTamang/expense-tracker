2cc78c2edbc505dd2301cca402f0d27bb3fc418b
AsokTamang <ashoktmg2058@gmail.com>
2026-01-13 00:46:39 -0500

    endpoint for addition of data done

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 import database
 from datetime import date
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class Expense(BaseModel):
+    expense_date:date
+    amount:int
+    category:str
+    notes:str
+
 
 @app.get('/expenses')
 async def get_expenses():
@@ -20,5 +29,10 @@ async def get_date_expense(expense_date:date):
 @app.post('/expenses/delete/{expense_date}')
 async def delete_date_expense(expense_date:date):
     expense = database.delete_expense_date_data(expense_date)
+    return expense
+
+@app.post('/expenses/insert')
+async def insert_date_expense(data:Expense):
+    expense = database.insert_into_database(expense_date=data.expense_date,amount=data.amount,category=data.category,notes=data.notes)
     return expense
 
