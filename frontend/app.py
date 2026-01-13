0f1c602edc449365709e424e72ec4ba9487e7897
AsokTamang <ashoktmg2058@gmail.com>
2026-01-13 13:28:52 -0500

    fetching the data based on date on form done

--- frontend/app.py
+++ frontend/app.py
@@ -11,30 +11,42 @@ tab1,tab2=st.tabs(['Add/Update','Analytics'])
 categories = ['Rent','Food','Shopping','Entertainment','Other']
 with tab1:
     selected_date=st.date_input('Select Date',label_visibility='collapsed')
-    response = rq.get(f'{api}/expenses/{selected_date}')
+    response = rq.get(f'{api}/expenses/{selected_date.strftime("%Y-%m-%d")}')
     if response.status_code == 200:
         data = response.json()
+
 
     else:
         st.error('No expenses found on this date')
         data = []
 
     with st.form(key='expenses'):
-        column1, column2,column3 = st.columns(3)  #here we are makign 3 columns
+        column1, column2,column3 = st.columns(3)  #here we are making 3 columns
         with column1:
             st.text('Amount')
         with column2:
             st.text('Category')
         with column3:
             st.text('Notes')
-        for i in range(5):
-            with column1:
-                st.number_input('Amount',min_value=0.0,step=1.0,value=0.0,key=f'amount_{i}',label_visibility='collapsed')
-            with column2:
-                st.selectbox('Category',options =categories,key=f'category_{i}' ,label_visibility='collapsed')
-            with column3:
-               st.text_input(label='',value=" ",key=f'notes_{i}',label_visibility='collapsed')
 
-        st.form_submit_button('Submit')
+        for i in range(5):
+            if i<len(data):
+                st.session_state[f'amount_{i}'] = data[i]['amount']    #here we are changing the value of the columns using the session_state with the help of key
+                st.session_state[f'category_{i}'] = data[i]['category']
+                st.session_state[f'notes_{i}'] = data[i]['notes']
+            else:
+                st.session_state[f'amount_{i}'] = 0.0
+                st.session_state[f'category_{i}'] = "Rent"  #here we are making the rent category as default
+                st.session_state[f'notes_{i}'] = ""
+
+            with column1:
+                st.number_input(label='Amount',min_value=0.0,step=1.0,key=f'amount_{i}',label_visibility='collapsed')
+            with column2:
+                st.selectbox(label='Category',options =categories  ,key=f'category_{i}' ,label_visibility='collapsed')
+            with column3:
+               st.text_input(label='Notes',key=f'notes_{i}',label_visibility='collapsed')
+
+        submit_button=st.form_submit_button('Submit')
+
 
 
