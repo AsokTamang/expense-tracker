84dceabdf212f1fc4fa4eccb84dc6da63a6a8a50
AsokTamang <ashoktmg2058@gmail.com>
2026-01-13 00:37:26 -0500

    deletion endpoint done

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import database
+from datetime import date
 
 app = FastAPI()
 
@@ -7,4 +8,17 @@ app = FastAPI()
 async def get_expenses():
     expenses =  database.get_all_datas()
     return expenses
+
+#getting data from specific date
+@app.get('/expenses/{expense_date}')
+async def get_date_expense(expense_date:date):
+    expense = database.get_expense_date_data(expense_date)
+    return expense
+
+
+#deleting the data based on specific date
+@app.post('/expenses/delete/{expense_date}')
+async def delete_date_expense(expense_date:date):
+    expense = database.delete_expense_date_data(expense_date)
+    return expense
 
